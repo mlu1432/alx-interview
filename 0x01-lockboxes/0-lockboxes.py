cb484e4de6c4d9c12293846a9797914d506617c1
Lucas Sekwati <luxturesekwati@gmail.com>
2024-08-15 23:13:02 +0200

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,18 +2,27 @@
 """
 This module contains a function `canUnlockAll` that determines if all the boxes in a list
 can be unlocked starting from the first box (boxes[0]).
+
+Parameters:
+- boxes (list of lists): A list where each element is a list of integers representing the keys found in each box.
+
 Returns:
 - bool: True if all boxes can be opened, otherwise False.
 """
 def canUnlockAll(boxes):
-    opened_boxes = set()
-    keys = [0]
+    if not boxes:
+        return False
+
+    opened_boxes = set([0])
+    keys = list(boxes[0])
 
     while keys:
         current_key = keys.pop()
 
-        if current_key not in opened_boxes:
+ 
+        if current_key < len(boxes) and current_key not in opened_boxes:
             opened_boxes.add(current_key)
             keys.extend(boxes[current_key])
+
 
     return len(opened_boxes) == len(boxes)
